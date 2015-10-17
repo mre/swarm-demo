5e0387032177c83f2b6978b6e569b9925f11e57c
jsacha <jakub.sacha@trivago.com>
2015-10-17 23:38:39 +0200

    Fix IP and Port issue

--- start_base.sh
+++ start_base.sh
@@ -15,8 +15,8 @@ INFLUXDB_ID=`docker ps --filter name=influxdb -q`
 INFLUXDB_IP=`docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{range $p2, $c2 := $conf}}    {{$c2.HostIp}}{{end}}{{end}}' $HAPROXY_ID | awk '{print $1}'`
 
 ## register service in consul
-curl -X PUT -d "{\"ID\": \"$INFLUXDB_ID\", \"Name\": \"api.influxdb\", \"Address\": \"$INFLUXDB_IP\", \"Port\": 8086, \"Check\": { \"HTTP\": \"http://$CONTAINER_HOST:8086/query?q=SHOW%20DATABASES\", \"Interval\": \"2s\"  }}" http://$CONSUL_IP:8500/v1/agent/service/register
-curl -X PUT -d "{\"ID\": \"$INFLUXDB_ID\", \"Name\": \"graphite.influxdb\", \"Address\": \"$INFLUXDB_IP\", \"Port\": 2003, \"Check\": { \"HTTP\": \"http://$CONTAINER_HOST:8086/query?q=SHOW%20DATABASES\", \"Interval\": \"2s\"  }}" http://$CONSUL_IP:8500/v1/agent/service/register
+curl -X PUT -d "{\"ID\": \"$INFLUXDB_ID\", \"Name\": \"api.influxdb\", \"Address\": \"$INFLUXDB_IP\", \"Port\": 8086, \"Check\": { \"HTTP\": \"http://$INFLUXDB_IP:8086/query?q=SHOW%20DATABASES\", \"Interval\": \"2s\"  }}" http://$CONSUL_IP:8500/v1/agent/service/register
+curl -X PUT -d "{\"ID\": \"$INFLUXDB_ID\", \"Name\": \"graphite.influxdb\", \"Address\": \"$INFLUXDB_IP\", \"Port\": 2003, \"Check\": { \"HTTP\": \"http://$INFLUXDB_IP:8086/query?q=SHOW%20DATABASES\", \"Interval\": \"2s\"  }}" http://$CONSUL_IP:8500/v1/agent/service/register
 
 echo "/etc/hosts entries:"
 ## gets its ip and port
