f5a4173802cde2a5a2aa1c092750254b5b511bba
jsacha <jakub.sacha@trivago.com>
2015-10-19 12:05:45 +0200

    Use swarm-demo:server, talk to consul-dns

--- scale_up.sh
+++ scale_up.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+## GET CONSUL IP
+CONSUL_ID=`docker ps --filter name=consul -q`
+CONSUL_IP=`docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{range $p2, $c2 := $conf}}{{$c2.HostIp}} {{end}}{{end}}' $CONSUL_ID | awk '{print \$1}'`
+
+
 ## start new server instance
-NEW_CONTAINER_ID=`docker run -d -p 9090 nginx`
+NEW_CONTAINER_ID=`docker run --dns=$CONSUL_IP -d -p 9090 mre0/swarm-demo:server`
 
 ## gets its ip and port
 CONTAINER_HOST=`docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{range $p2, $c2 := $conf}}{{$c2.HostIp}}{{end}}{{end}}' $NEW_CONTAINER_ID`
 CONTAINER_PORT=`docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{range $p2, $c2 := $conf}}{{$c2.HostPort}}{{end}}{{end}}' $NEW_CONTAINER_ID`
 
-## GET CONSUL IP
-CONSUL_ID=`docker ps --filter name=consul -q`
-CONSUL_IP=`docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{range $p2, $c2 := $conf}}{{$c2.HostIp}} {{end}}{{end}}' $CONSUL_ID | awk '{print \$1}'`
 
 ## register service in consul
 curl -X PUT -d "{\"ID\": \"$NEW_CONTAINER_ID\", \"Name\": \"primeserver\", \"Address\": \"$CONTAINER_HOST\", \"Port\": $CONTAINER_PORT, \"Check\": { \"HTTP\": \"http://$CONTAINER_HOST:$CONTAINER_PORT/\", \"Interval\": \"2s\"  }}" http://$CONSUL_IP:8500/v1/agent/service/register
