354348758b88dcd57e9095309d18d7054f9c3a23
jsacha <jakub.sacha@trivago.com>
2015-10-19 16:40:18 +0200

    Add delay when spinning up new server

--- scale_up.sh
+++ scale_up.sh
@@ -4,7 +4,6 @@
 CONSUL_ID=`docker ps --filter name=consul -q`
 CONSUL_IP=`docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{range $p2, $c2 := $conf}}{{$c2.HostIp}} {{end}}{{end}}' $CONSUL_ID | awk '{print \$1}'`
 
-
 ## start new server instance
 NEW_CONTAINER_ID=`docker run --dns=$CONSUL_IP -d -p 9090 mre0/swarm-demo:server`
 
@@ -12,6 +11,8 @@ NEW_CONTAINER_ID=`docker run --dns=$CONSUL_IP -d -p 9090 mre0/swarm-demo:server`
 CONTAINER_HOST=`docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{range $p2, $c2 := $conf}}{{$c2.HostIp}}{{end}}{{end}}' $NEW_CONTAINER_ID`
 CONTAINER_PORT=`docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{range $p2, $c2 := $conf}}{{$c2.HostPort}}{{end}}{{end}}' $NEW_CONTAINER_ID`
 
+## give docker 1 sec to spin up the container
 
+sleep 2
 ## register service in consul
-curl -X PUT -d "{\"ID\": \"$NEW_CONTAINER_ID\", \"Name\": \"primeserver\", \"Address\": \"$CONTAINER_HOST\", \"Port\": $CONTAINER_PORT, \"Check\": { \"HTTP\": \"http://$CONTAINER_HOST:$CONTAINER_PORT/\", \"Interval\": \"60s\"  }}" http://$CONSUL_IP:8500/v1/agent/service/register
+curl -X PUT -d "{\"ID\": \"$NEW_CONTAINER_ID\", \"Name\": \"primeserver\", \"Address\": \"$CONTAINER_HOST\", \"Port\": $CONTAINER_PORT, \"Check\": { \"HTTP\": \"http://$CONTAINER_HOST:$CONTAINER_PORT/health\", \"Interval\": \"60s\"  }}" http://$CONSUL_IP:8500/v1/agent/service/register
